0af1e71a031329e6fb94a098091ee30e75e6ddcd
cyberron2021 <geronimo.mananghaya@gmail.co>
2022-07-20 20:50:31 +0800

    deploy game link

--- src/pages/Games/Games.tsx
+++ src/pages/Games/Games.tsx
@@ -34,6 +34,7 @@ import {
 import React, { useContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router";
+import { Link, NavLink } from "react-router-dom";
 import isLoggedIn from "../../components/Login/isLoggedIn";
 import { getGamelist } from "../../redux/actions/games";
 import "./Games.css";
@@ -156,28 +157,35 @@ const Games: React.FC = () => {
                     )
                 )
                 .map((item: any, index: any) => (
-                  <IonCard key={index}>
-                    <IonCardContent>
-                      <div className="content-wrapper">
-                        <img className="img" src={item.thumbnail} />
+                  <NavLink
+                    style={{ textDecoration: "none" }}
+                    key={index}
+                    to={{ pathname: item.game_url }}
+                    target="_blank"
+                  >
+                    <IonCard key={index}>
+                      <IonCardContent>
+                        <div className="content-wrapper">
+                          <img className="img" src={item.thumbnail} />
 
-                        <div className="desc-wrapper">
-                          <div className="title">
-                            <span>{item.title}</span>
-                          </div>
-                          <div className="genre">Genre: {item.genre}</div>
-                          <div className="platform">Platform:</div>
-                          <div className="platform-logo">
-                            {callLogo(item.platform)}
+                          <div className="desc-wrapper">
+                            <div className="title">
+                              <span>{item.title}</span>
+                            </div>
+                            <div className="genre">Genre: {item.genre}</div>
+                            <div className="platform">Platform:</div>
+                            <div className="platform-logo">
+                              {callLogo(item.platform)}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="description">
-                        Description:
-                        {item.short_description}
-                      </div>
-                    </IonCardContent>
-                  </IonCard>
+                        <div className="description">
+                          Description:
+                          {item.short_description}
+                        </div>
+                      </IonCardContent>
+                    </IonCard>
+                  </NavLink>
                 ))
             ) : (
               <IonCard>
